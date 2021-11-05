08ff993cc7cf3e742aad09a7fe3f4a5d1bd1d15d
Lino Alfonso <lmena@kubo.co>
2021-11-05 14:13:20 -0500

    fix implements injection dependency

--- app/src/main/java/com/lino/shopcart/database/ShopCartMovieDataBase.kt
+++ app/src/main/java/com/lino/shopcart/database/ShopCartMovieDataBase.kt
@@ -10,21 +10,5 @@ import com.lino.shopcart.models.Movie
 @Database(entities = arrayOf(Movie::class),version = 1)
 abstract class ShopCartMovieDataBase: RoomDatabase() {
     abstract fun movieDao():MovieDao
-   /* companion object{
-        @Volatile
-        private var instance : ShopCartMovieDataBase? = null
-        private var LOCK = Any()
 
-        operator fun invoke (context: Context) = instance ?: synchronized(LOCK){
-                instance ?: createDatabase(context).also {
-                    instance = it
-                }
-        }
-
-        private fun createDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            ShopCartMovieDataBase::class.java,
-            "shopCartMovie_db.db"
-        ).build()
-    }*/
 }
